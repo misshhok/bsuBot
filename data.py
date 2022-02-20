2566e65c1c15a7db3311438fab35bd33a7914089
misshhok <mishasmirnov03@mail.ru>
2022-02-21 02:48:25 +0300

    bsuBot 1.1

--- data.py
+++ data.py
@@ -1,8 +1,4 @@
-import datetime
 payload = {  # словарь с входными данными
     'username': '1559859',
     'password': '2017076826Mishasuper1'
 }
-now = datetime.datetime.now()
-print(str(now))
-print(now.strftime("%H:%M"))
